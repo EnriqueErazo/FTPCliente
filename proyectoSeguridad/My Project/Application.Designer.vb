394795641927c4305c7a2974858d6678a1ff40fc
EnriqueErazo <enrique.erazop@gmail.com>
2017-06-09 22:30:47 -0600

    Cambio de nombre

--- proyectoSeguridad/My Project/Application.Designer.vb
+++ proyectoSeguridad/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.proyectoSeguridad.frmLogin
+            Me.MainForm = Global.proyectoSeguridadCliente.frmLogin
         End Sub
     End Class
 End Namespace
